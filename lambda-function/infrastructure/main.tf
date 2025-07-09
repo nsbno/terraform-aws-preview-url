b384fd151e3f9ca15ef91253dee1981ded803784
Wictor Zhao <wictor_x@hotmail.com>
2025-07-09 08:17:59 +0200

    Refactor infrastructure for centralized infra to only contain DDB

--- lambda-function/infrastructure/main.tf
+++ lambda-function/infrastructure/main.tf
@@ -1,47 +1,45 @@
 locals {
   service_name = "preview-url-mapper"
 
-  lambda_at_edge_artifact_bucket = "727832596008-lambda-at-edge-artifacts"
-  s3_artifact_path = "${local.service_name}/main.zip"
+  # lambda_at_edge_artifact_bucket = "727832596008-lambda-at-edge-artifacts"
+  # s3_artifact_path               = "${local.service_name}/main.zip"
 
-  artifact = {
-	store = local.lambda_at_edge_artifact_bucket
-	path    = local.s3_artifact_path
-	version = data.aws_s3_object.latest_artifact.version_id
-  }
+  # artifact = {
+  #   store   = local.lambda_at_edge_artifact_bucket
+  #   path    = local.s3_artifact_path
+  #   version = data.aws_s3_object.latest_artifact.version_id
+  # }
 }
 
-data "aws_s3_object" "latest_artifact" {
-  bucket = local.lambda_at_edge_artifact_bucket
-  key    = local.s3_artifact_path
-}
+# data "aws_s3_object" "latest_artifact" {
+#   bucket = local.lambda_at_edge_artifact_bucket
+#   key    = local.s3_artifact_path
+# }
 
-module "lambda" {
-  source = "github.com/nsbno/terraform-aws-lambda?ref=codedeploy"
+# module "lambda" {
+#   source = "github.com/nsbno/terraform-aws-lambda?ref=codedeploy"
+#
+#   service_name = local.service_name
+#
+#   artifact      = local.artifact
+#   artifact_type = "s3"
+#
+#   # Name correspond to built artifact
+#   handler = "preview_url_mapper.handler.get_preview_url_by_domain"
+#   runtime = "python3.13"
+#
+#   memory = 256
+# }
 
-  service_name = local.service_name
-
-  artifact      = local.artifact
-  artifact_type = "s3"
-
-  # Name correspond to built artifact
-  handler = "preview_url_mapper.handler.get_preview_url_by_domain"
-  runtime = "python3.13"
-
-  memory = 256
-}
-
-data "aws_organizations_organization" "this" {}
-
-resource "aws_lambda_permission" "allow_org_invoke" {
-  statement_id  = "AllowOrgInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = module.lambda.function_name
-  principal     = "*"
-  qualifier     = module.lambda.function_qualifier
-
-  principal_org_id = data.aws_organizations_organization.this.id
-}
+# resource "aws_lambda_permission" "allow_org_invoke" {
+#   statement_id  = "AllowOrgInvoke"
+#   action        = "lambda:InvokeFunction"
+#   function_name = module.lambda.function_name
+#   principal     = "*"
+#   qualifier     = module.lambda.function_qualifier
+#
+#   principal_org_id = data.aws_organizations_organization.this.id
+# }
 
 module "dynamodb" {
   source = "github.com/nsbno/terraform-aws-dynamodb?ref=1.0.3"
@@ -53,15 +51,40 @@ module "dynamodb" {
   ttl_attribute = "timestamp"
 }
 
-module "permissions" {
-  source = "github.com/nsbno/terraform-aws-service-permissions?ref=1.2.0"
+data "aws_organizations_organization" "this" {}
 
-  role_name = module.lambda.role_name
+data "aws_iam_policy_document" "org_access" {
+  statement {
+    effect = "Allow"
 
-  dynamodb_tables = [
-    {
-      arns        = [module.dynamodb.arn]
-      permissions = ["put", "get", "delete"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
     }
-  ]
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:Scan",
+      "dynamodb:Query"
+    ]
+
+    resources = [
+      module.dynamodb.arn,
+      "${module.dynamodb.arn}/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalOrgID"
+      values   = [data.aws_organizations_organization.this.id]
+    }
+  }
+}
+
+resource "aws_dynamodb_resource_policy" "org_access" {
+  resource_arn = module.dynamodb.arn
+  policy       = data.aws_iam_policy_document.org_access.json
 }
