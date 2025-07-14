5d3d3e145c734bf0877b92dd2ca50b299ec3a893
Wictor Zhao <wictor_x@hotmail.com>
2025-07-14 12:24:38 +0200

    Use native boto3 for dynamodb

--- lambda-function/preview_url_mapper/adapters/dynamodb_adapter.py
+++ lambda-function/preview_url_mapper/adapters/dynamodb_adapter.py
@@ -1,34 +1,35 @@
-from datetime import datetime, timedelta
-
-from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute
-from pynamodb.models import Model
+import boto3
 
 from preview_url_mapper.ports.preview_url_repository import PreviewUrlRepository
 
 
 class DynamoDBPreviewUrlAdapter(PreviewUrlRepository):
-    # us-east-1 for the same region as Lambda@Edge function
-    def __init__(self, table_name: str, region='us-east-1') -> None:
-        self.table_name = table_name
-        self.region = region
+    def __init__(
+            self,
+            table_name: str,
+            region: str = 'us-east-1',
+            account_id: str = "846274634169"  # vydeployment-test
+    ) -> None:
+        self.dynamodb = boto3.resource('dynamodb', region_name=region)
 
-        class PreviewUrlModel(Model):
-            class Meta:
-                table_name = self.table_name
-                region = self.region
-
-            domain = UnicodeAttribute(hash_key=True)
-            pr_number = UnicodeAttribute()
-            apprunner_host = UnicodeAttribute()
-            timestamp = UTCDateTimeAttribute(default=datetime.now() + timedelta(days=7))
-
-        self.PreviewUrlModel = PreviewUrlModel
+        self.table_arn = f"arn:aws:dynamodb:{region}:{account_id}:table/{table_name}"
+        self.table = self.dynamodb.Table(name=self.table_arn)
 
     def get_preview_url(self, domain: str, pr_id: str) -> str:
         try:
-            apprunner_host = self.PreviewUrlModel.get(hash_key=domain).apprunner_host
-            return apprunner_host
-        except self.PreviewUrlModel.DoesNotExist:
+            response = self.table.get_item(
+
+                Key={
+                    'domain': domain
+                }
+            )
+
+            if 'Item' not in response:
+                raise ValueError(f"No preview URL found for domain: {domain}")
+
+            return response['Item']['apprunner_host']
+
+        except self.dynamodb.meta.client.exceptions.ResourceNotFoundException:
             raise ValueError(f"No preview URL found for domain: {domain}")
         except Exception as e:
             raise ValueError(f"Error: {str(e)}") from e
